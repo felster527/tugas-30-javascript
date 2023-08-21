e7836a786fbd16d01c79a3e3088903324b0c34b3
felster527 <91239878+felster527@users.noreply.github.com>
2023-08-21 20:00:35 +0700

    Add files via upload

---
+++ Tugas30.js
@@ -0,0 +1,9 @@
+function panggilRegex(x){
+    let ambildata = x.match(/[e,s]/g)
+
+    return ambildata
+}
+
+let str = "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat"
+
+console.log(Regex(str))
